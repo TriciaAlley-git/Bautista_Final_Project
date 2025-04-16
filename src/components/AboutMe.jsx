658ec17e6959207f3bcb74eca31dbd26f66df351
unknown <triciaalleybautista@gmail.com>
2025-04-16 16:35:53 +0800

    Initial Commit

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AboutMe = () => {
+  return (
+    <section className="my-12">
+      <h2 className="text-3xl font-semibold text-blue-600">About Me</h2>
+      <p className="text-lg mt-4">
+        Hi I am a 2nd year Bachelor of Information Technology student learning web application development this semester, creating artistic and interactive websites. This portfolio showcases my growth and creative projects.
+      </p>
+    </section>
+  );
+};
+
+export default AboutMe;
